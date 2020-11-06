7aa253e99461a42c16b8baeae704c4c9b826a6fc
khoirul <khoirulhudaid@gmail.com>
2020-11-06 20:06:18 +0700

    make auth

--- Siakad/database/migrations/2020_11_06_105311_create_admins.php
+++ Siakad/database/migrations/2020_11_06_105311_create_admins.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAdmins extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('npm');
-            $table->string('password');
-            $table->date('TTL');
-            $table->string('alamat');
-            $table->string('kategori');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -33,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 }
