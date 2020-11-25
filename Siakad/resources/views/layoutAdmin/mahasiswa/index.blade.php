5f87988d38a1c593a202c3e6c57917ba20eeda5d
Achmad Muchlasin <47469965+muchlaseen@users.noreply.github.com>
2020-11-25 15:42:11 +0700

    Update modal form: add mahasiswa

--- Siakad/resources/views/layoutAdmin/mahasiswa/index.blade.php
+++ Siakad/resources/views/layoutAdmin/mahasiswa/index.blade.php
@@ -26,7 +26,7 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Tambah Data Mahasiswa</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
@@ -34,17 +34,47 @@
                         <div class="modal-body">
                             <form>
                                 <div class="form-group">
-                                  <label for="exampleInputEmail1">Email address</label>
-                                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                  <label for="inputNPM">NPM</label>
+                                  <input type="text" class="form-control" id="inputNPM" aria-describedby="emailHelp">
                                 </div>
                                 <div class="form-group">
-                                  <label for="exampleInputPassword1">Password</label>
-                                  <input type="password" class="form-control" id="exampleInputPassword1">
+                                    <label for="inputJurusan">ID Jurusan</label>
+                                    <input type="text" class="form-control" id="inputJurusan" aria-describedby="emailHelp">
                                 </div>
-                                <div class="form-group form-check">
-                                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                <div class="form-group">
+                                    <label for="inputNama">Nama</label>
+                                    <input type="text" class="form-control" id="inputNama" aria-describedby="emailHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Alamat</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                      <option>Laki-laki</option>
+                                      <option>Perempuan</option>
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputNama">Email</label>
+                                    <input type="text" class="form-control" id="inputNama" aria-describedby="emailHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputNama">No Telfon</label>
+                                    <input type="text" class="form-control" id="inputNama" aria-describedby="emailHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputNama">Tempat Lahir</label>
+                                    <input type="text" class="form-control" id="inputNama" aria-describedby="emailHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputNama">Tanggal Lahir</label>
+                                    <input type="date" class="form-control" id="inputNama" aria-describedby="emailHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputNama">Tempat Lahir</label>
+                                    <input type="text" class="form-control" id="inputNama" aria-describedby="emailHelp">
                                 </div>
                                 <button type="submit" class="btn btn-primary">Submit</button>
                               </form>
